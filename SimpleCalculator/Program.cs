8b20ccc53549b8dff8a6761194951c36897b5ccd
Jamena McInteer <jamena@webdevabq.com>
2019-04-24 14:28:33 -0600

    initial commit

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+                InputConverter inputConverter = new InputConverter(); //InputCoverter is our custom type
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                // TODO: log exceptions
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
